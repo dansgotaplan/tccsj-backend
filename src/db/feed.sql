ff308dd45c4fe9d96672383a15d96d6057f57f39
Daniel Souza <viniciusdanielsalves@gmail.com>
2025-08-15 10:16:21 -0300

    ci(database): Adicionar polos na Database

--- src/db/feed.sql
+++ src/db/feed.sql
@@ -1,18 +1,236 @@
-INSERT INTO usuario(email, senha, isadmin) VALUES ('viniciusdanielsalves', 'vascaodagranatacommoney', 1);
+INSERT INTO usuario(email, senha, isadmin) VALUES (
+  'viniciusdanielsalves@gmail.com',
+  'vascaodagranatacommoney',
+  1
+);
 
-INSERT INTO tag(id, nome) VALUES ('mscforro', 'Forro');
-INSERT INTO tag(id, nome) VALUES ('mscsertanejo', 'Sertanejo');
-INSERT INTO tag(id, nome) VALUES ('mscfunk', 'Funk');
-INSERT INTO tag(id, nome) VALUES ('mscbregafunk', 'Brega Funk');
-INSERT INTO tag(id, nome) VALUES ('mscsertanejouni', 'Sertanejo Universitario');
-INSERT INTO tag(id, nome) VALUES ('mscgospel', 'Gospel');
-INSERT INTO tag(id, nome) VALUES ('mascbrega', 'Brega');
-INSERT INTO tag(id, nome) VALUES ('mscsofrencia', 'Sofrencia');
-INSERT INTO tag(id, nome) VALUES ('mscpagode', 'Pagode');
-INSERT INTO tag(id, nome) VALUES ('mscsamba', 'Samba');
-INSERT INTO tag(id, nome) VALUES ('mscrap', 'Rap');
-INSERT INTO tag(id, nome) VALUES ('msctrap', 'Trap');
-INSERT INTO tag(id, nome) VALUES ('mscinternacional', 'Internacional');
-INSERT INTO tag(id, nome) VALUES ('msceletronica', 'Eletronica');
-INSERT INTO tag(id, nome) VALUES ('mscpop', 'Pop');
-INSERT INTO tag(id, nome) VALUES ('mscrock', 'Rock');
+INSERT INTO tag(id, nome) VALUES (
+  'mscforro', 
+  'Forro'
+);
+
+INSERT INTO tag(id, nome) VALUES (
+  'mscsertanejo',
+  'Sertanejo'
+);
+
+INSERT INTO tag(id, nome) VALUES (
+  'mscfunk',
+  'Funk'
+);
+
+INSERT INTO tag(id, nome) VALUES (
+  'mscbregafunk',
+  'Brega Funk'
+);
+
+INSERT INTO tag(id, nome) VALUES (
+  'mscsertanejouni',
+  'Sertanejo Universitario'
+);
+
+INSERT INTO tag(id, nome) VALUES (
+  'mscgospel',
+  'Gospel'
+);
+
+INSERT INTO tag(id, nome) VALUES (
+  'mascbrega',
+  'Brega'
+);
+
+INSERT INTO tag(id, nome) VALUES (
+  'mscsofrencia',
+  'Sofrencia'
+);
+
+INSERT INTO tag(id, nome) VALUES (
+  'mscpagode',
+  'Pagode'
+);
+
+INSERT INTO tag(id, nome) VALUES (
+  'mscsamba',
+  'Samba'
+);
+
+INSERT INTO tag(id, nome) VALUES (
+  'mscrap',
+  'Rap'
+);
+
+INSERT INTO tag(id, nome) VALUES (
+  'msctrap',
+  'Trap'
+);
+
+INSERT INTO tag(id, nome) VALUES (
+  'mscinternacional',
+  'Internacional'
+);
+
+INSERT INTO tag(id, nome) VALUES (
+  'msceletronica',
+  'Eletronica'
+);
+
+INSERT INTO tag(id, nome) VALUES (
+  'mscpop',
+  'Pop'
+);
+
+INSERT INTO tag(id, nome) VALUES (
+  'mscrock',
+  'Rock'
+);
+
+INSERT INTO polo(id, nome, descricao, inicio, fim, endereco, latitude, longitude) VALUES (
+  'juarezsantiago',
+  'Polo Juarez Santiago',
+  'Polo dedicado ao forró tradicional pé-de-serra, localizado no Espaço Tancredo Neves, Centro de Caruaru.',
+  '2025-05-31',
+  '2025-06-28',
+  'Espaço Tancredo Neves, Centro, Caruaru-PE',
+  -8.284547,
+  -35.969863
+);
+
+INSERT INTO polo(id, nome, descricao, inicio, fim, endereco, latitude, longitude) VALUES (
+  'sebastiaobiano',
+  'Polo Sebastião Biano',
+  'Polo dedicado às apresentações das bandas de pífanos, mantendo viva uma tradição centenária. Localizado na Estação Ferroviária.',
+  '2025-05-31',
+  '2025-06-28',
+  'Estação Ferroviária, Caruaru-PE',
+  -8.28139,
+  -35.9735
+);
+
+INSERT INTO polo(id, nome, descricao, inicio, fim, endereco, latitude, longitude) VALUES (
+  'camarao',
+  'Polo Camarão',
+  'Polo de destaque na Estação Ferroviária, com diversas atrações musicais e culturais, focado no forró pé de serra.',
+  '2025-05-31',
+  '2025-06-28',
+  'Estação Ferroviária, Caruaru-PE',
+  -8.28139,
+  -35.9735
+);
+
+INSERT INTO polo(id, nome, descricao, inicio, fim, endereco, latitude, longitude) VALUES (
+  'lidiocavalcanti',
+  'Polo Lídio Cavalcanti',
+  'Conhecido como Polo do Repente, atrai amantes da poesia e do forró tradicional. Localizado na Estação Ferroviária.',
+  '2025-05-31',
+  '2025-06-28',
+  'Estação Ferroviária, Caruaru-PE',
+  -8.28139,
+  -35.9735
+);
+
+INSERT INTO polo(id, nome, descricao, inicio, fim, endereco, latitude, longitude) VALUES (
+  'infantil',
+  'Polo Infantil',
+  'Espaço dedicado à diversão das crianças, com atividades lúdicas, teatro infantil e shows musicais. Localizado na Estação Ferroviária.',
+  '2025-05-31',
+  '2025-06-28',
+  'Estação Ferroviária, Caruaru-PE',
+  -8.28139,
+  -35.9735
+);
+
+INSERT INTO polo(id, nome, descricao, inicio, fim, endereco, latitude, longitude) VALUES (
+  'rildohora',
+  'Polo Rildo Hora',
+  'Polo instrumental localizado no Monte Bom Jesus, com shows de diversos estilos musicais e infraestrutura completa.',
+  '2025-05-31',
+  '2025-06-28',
+  'Monte Bom Jesus, Caruaru-PE',
+  -8.28333,
+  -35.96667
+);
+
+INSERT INTO polo(id, nome, descricao, inicio, fim, endereco, latitude, longitude) VALUES (
+  'feiradeartesanato'
+  'Feira de Artesanato',
+  'Local onde se encontra uma variedade de produtos artesanais, comidas típicas e apresentações culturais. Funciona no Parque 18 de Maio.',
+  '2025-05-31',
+  '2025-06-28',
+  'Parque 18 de Maio, Av. Lourival José da Silva, Petrópolis, Caruaru-PE',
+  -8.28139,
+  -35.9735
+);
+
+INSERT INTO polo(id, nome, descricao, inicio, fim, endereco, latitude, longitude) VALUES (
+  'bacamartes',
+  'Bacamarte',
+  'Polo que celebra a tradição dos bacamarteiros, com desfiles e tiros de bacamarte, simbolizando a força e identidade nordestina. Localizado na Estação Ferroviária.',
+  '2025-05-31',
+  '2025-06-28',
+  'Estação Ferroviária, Caruaru-PE',
+  -8.28139,
+  -35.9735
+);
+
+INSERT INTO polo(id, nome, descricao, inicio, fim, endereco, latitude, longitude) VALUES (
+  'patiodeventos',
+  'Pátio de Eventos',
+  'Principal polo do São João de Caruaru, conhecido como Pátio de Eventos Luiz Gonzaga, com grandes shows e atrações.',
+  '2025-05-31',
+  '2025-06-28',
+  'Pátio de Eventos Luiz Gonzaga, Caruaru-PE',
+  -8.284547,
+  -35.969863
+);
+
+INSERT INTO polo(id, nome, descricao, inicio, fim, endereco, latitude, longitude) VALUES (
+  'azulao',
+  'Polo Azulão',
+  'Palco dedicado à música alternativa do São João de Caruaru, com programação diversificada.',
+  '2025-05-31',
+  '2025-06-28',
+  'Caruaru-PE',
+  -8.28333,
+  -35.96667
+);
+
+INSERT INTO polo(id, nome, descricao, inicio, fim, endereco, latitude, longitude) VALUES (
+  'altodomoura',
+  'Alto do Moura',
+  'Distrito de Caruaru que se destaca como um dos principais centros de cultura popular e artesanato do Nordeste, com programação dedicada ao forró.',
+  '2025-05-31',
+  '2025-06-28',
+  'Alto do Moura, Caruaru-PE',
+  -8.28333,
+  -35.96667
+);
+
+INSERT INTO polo(id, nome, descricao, inicio, fim, endereco, latitude, longitude) VALUES (
+  'casarosa',
+  'Casa Rosa',
+  'Mercado Cultural e Gastronômico, ponto de encontro durante o dia com forró e gastronomia. Localizado no Parque 18 de Maio.',
+  '2025-05-31',
+  '2025-06-28',
+  'Parque 18 de Maio, S/N - Petrópolis, Caruaru-PE',
+  -8.28139,
+  -35.9735
+);
+
+INSERT INTO polo(id, nome, descricao, inicio, fim, endereco, latitude, longitude) VALUES (
+  'Polo das Quadrilhas',
+  'Espaço dedicado às apresentações de quadrilhas juninas, localizado no Centro Multicultural de Caruaru, no Alto do Moura.',
+  '2025-06-07',
+  '2025-06-28',
+  'Centro Multicultural de Caruaru, Alto do Moura, Caruaru-PE',
+  -8.28333,
+  -35.96667
+);
+
+INSERT INTO polo(id, nome, descricao, inicio, fim, ismultilocal) VALUES (
+  'sjnaroca',
+  'São João na Roça',
+  'Uma série de exibições juninas nas comunidades rurais de Caruaru',
+  '2025-04-25',
+  '2025-05-24',
+  1
+);
